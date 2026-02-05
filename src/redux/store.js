192644109ae429a09adc9f713ca0ba460c455df1
Nehal Kadyan <nehalkadyan@Nehals-MacBook-Air.local>
2026-02-05 10:20:11 +0530

    redux setup with reduxjs-toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,24 @@
 // Redux Global Store
+
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import userReducer from "./slices/userSlice"
+
+const rootReducer = combineReducers({
+    user : userReducer
+})
+
+// code for creating and exporting store using configureStore
+export const store = configureStore({
+    // reducers
+  reducer: rootReducer,
+  // middleware 
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+    // devtools - redux store (present)
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+
+
