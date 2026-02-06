aa3eb99cf2bfa65ebf2e44bcb5d4bdce9aef7fbf
Nehal Kadyan <nehalkadyan@Nehals-MacBook-Air.local>
2026-02-06 11:02:38 +0530

    create post completed

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,14 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { FaPen } from "react-icons/fa";
 import { IoIosSend } from "react-icons/io";
 
 const CreatePost = () => {
-    // state variable for post content
-    const [content, setContent] = useState("");
-    console.log("content",content);
+  // state variable for post content
+  const [content, setContent] = useState("");
+  console.log("content", content);
 
-    const [isSendButtonEnabled, setIsSendButtonEnabled] = useState(false)
-    // isSendButtonEnabled = false
+  const [isSendButtonEnabled, setIsSendButtonEnabled] = useState(false);
+  // isSendButtonEnabled = false
 
   return (
     <div>
@@ -19,18 +19,21 @@ const CreatePost = () => {
           className="p-4 text-2xl  outline-none"
           onChange={(e) => setContent(e.target.value)}
         />
-         {/* create post icon */}
-         <div>
+        {/* create post icon */}
+        <div>
+            {/* if false then show pen icon */}
+          {!isSendButtonEnabled && (
+            <FaPen
+              onClick={() => setIsSendButtonEnabled(true)}
+              className="text-2xl cursor-pointer"
+            />
+          )}
 
-    
-             <FaPen className="text-2xl"/>
-             
-             {
-                // is isSendButtonEnabled = true then only show send icon
-                isSendButtonEnabled && (<IoIosSend className="text-2xl"/>)
-             }
-             
-         </div>
+          {
+            // is isSendButtonEnabled = true then only show send icon
+            isSendButtonEnabled && <IoIosSend className="text-4xl cursor-pointer" />
+          }
+        </div>
       </div>
     </div>
   );
