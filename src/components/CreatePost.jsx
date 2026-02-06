c1d908b029f44697d9f5eacf27538b46a6b7c431
Nehal Kadyan <nehalkadyan@Nehals-MacBook-Air.local>
2026-02-06 10:53:07 +0530

    logic create post

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import { FaPen } from "react-icons/fa";
+import { IoIosSend } from "react-icons/io";
 
 const CreatePost = () => {
+    // state variable for post content
+    const [content, setContent] = useState("");
+    console.log("content",content);
+
+    const [isSendButtonEnabled, setIsSendButtonEnabled] = useState(false)
+    // isSendButtonEnabled = false
+
   return (
     <div>
       <div className="flex items-center border-2 border-gray-500 px-10 rounded-full py-2">
@@ -9,10 +17,19 @@ const CreatePost = () => {
           type="text"
           placeholder="What's on your mind?"
           className="p-4 text-2xl  outline-none"
+          onChange={(e) => setContent(e.target.value)}
         />
          {/* create post icon */}
          <div>
+
+    
              <FaPen className="text-2xl"/>
+             
+             {
+                // is isSendButtonEnabled = true then only show send icon
+                isSendButtonEnabled && (<IoIosSend className="text-2xl"/>)
+             }
+             
          </div>
       </div>
     </div>
