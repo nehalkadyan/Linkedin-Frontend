85e7434eb1c7f79e4f96ad7e481d86b3c11f3454
Nehal Kadyan <nehalkadyan@Nehals-MacBook-Air.local>
2026-02-06 10:16:47 +0530

    profile section icons

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -1,37 +1,48 @@
-import React from 'react'
-import {useSelector} from 'react-redux';
+import React from "react";
+import { useSelector } from "react-redux";
+import { CiUser } from "react-icons/ci";
+import { IoIosSettings } from "react-icons/io";
+import { TbPremiumRights } from "react-icons/tb";
+
+
 
 const ProfileSection = () => {
+  // currentUser
 
-    // currentUser 
-
-    const {currentUser} = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
 
   return (
-    <div className='border-2 flex flex-col items-center border-black rounded-lg p-10'>
-       <div className='flex items-center gap-2'>
+    <div className="border-2 flex flex-col items-center border-black rounded-lg p-10">
+      <div className="flex items-center gap-2">
         <img
-          className="w-17 ml-[-18px]"
+          className="w-17 ml-[-20px]"
           src="https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Image.png"
           alt="user_profile"
         />
-         <div>
-             <h2 className='font-semibold text-lg'>{currentUser?.username}</h2>
-         </div>
-       </div>
+        <div>
+          <h2 className="font-semibold text-xl">{currentUser?.username}</h2>
+          <h4 className="text-gray-500">headline</h4>
+        </div>
+      </div>
 
-       {/* profile menu items */}
+      {/* profile menu items */}
 
-{/* mt - margin top */}
-       <div className='mt-6'>
-          <ul className='text-2xl font-semibold flex flex-col gap-3'>
-            <li className='cursor-pointer'>Profile Views</li>
-            <li className='cursor-pointer'>Account settings</li>
-            <li className='cursor-pointer'>Upgrade to Premium</li>
-          </ul>
-       </div>
+      {/* mt - margin top */}
+      <div className="mt-6">
+        <ul className="text-2xl font-semibold flex flex-col gap-3">
+          <li className="cursor-pointer flex items-center gap-2">
+             <div>
+              <CiUser />
+            </div>
+            <div>Profile Views</div>{" "}
+          </li>
+          <li className="cursor-pointer flex items-center gap-2"> <div><IoIosSettings /></div>
+          <div>Account settings</div></li>
+          <li className="cursor-pointer flex items-center gap-2"><div><TbPremiumRights /></div>Upgrade to Premium</li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileSection
+export default ProfileSection;
